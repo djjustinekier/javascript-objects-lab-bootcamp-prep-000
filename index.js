5cc5dd112f95fc771659f5bf510f3ce091fa2060
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-07 02:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = delete object[key];
+  return newObj;
+}
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   delete newObj[key];
   return object;
-}
-function deleteFromObjectByKey(object, key) {
-  var newObj = delete object[key];
-  return newObj;
-
 }
 
 
