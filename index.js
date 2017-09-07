e71a5f6d0b98534e7dbf47b875f8d356cc1c10d8
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-07 02:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 }
 function deleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object;
 }
 function deleteFromObjectByKey(object, key) {
