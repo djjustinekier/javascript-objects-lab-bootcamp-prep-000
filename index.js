9edf0be49b5919047cc2c7d4415078478000da72
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-07 02:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
-  
+
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.'key' = value;
+  object[key] = value;
   return object;
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function deleteFromObjectByKey() {
-  
+
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
